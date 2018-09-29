d9ed480bfd1c1d5a7681d0dff5f819d8a4b2b45b
WYJ0309 <3116373546@qq.com>
2018-09-29 17:35:08 +0800

    添加管理中心首页，样式，js

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,10 +9,7 @@ def index():
 @blogBlue.route("/welcome",methods=["GET","POST"])
 def welcome():
 
-    render_template("welcome.html")
-
-
-
+    return render_template("welcome.html")
 
 
 @blogBlue.route("/register",methods=["GET","POST"])
@@ -44,3 +41,42 @@ def login():
         else:
             msg = '用户名或者密码错误'
     return render_template('login.html', msg=msg)
+
+
+
+@blogBlue.route("/demo/btn")
+def btn():
+
+    return render_template("/demo/btn.html")
+@blogBlue.route("/demo/form")
+def form():
+
+    return render_template("/demo/form.html")
+@blogBlue.route("/demo/table")
+def table():
+
+    return render_template("/demo/table.html")
+@blogBlue.route("/demo/tab_card")
+def tab_card():
+
+    return render_template("/demo/tab_card.html")
+@blogBlue.route("/demo/progress_bar")
+def progress_bar():
+
+    return render_template("/demo/progress_bar.html")
+@blogBlue.route("/demo/folding_panel")
+def folding_panel():
+
+    return render_template("/demo/folding_panel.html")
+@blogBlue.route("/demo/auxiliar")
+def auxiliar():
+
+    return render_template("/demo/auxiliar.html")
+@blogBlue.route("/demo/add_edit")
+def add_edit():
+
+    return render_template("/demo/add_edit.html")
+@blogBlue.route("/demo/data_table")
+def data_table():
+
+    return render_template("/demo/data_table.html")
